c99f3b4f4e93eb75ffa5b97b1a1eaf79b9f3f218
sahil-diwan <isahildiwan101@gmail.com>
2020-06-30 17:51:47 +0530

    Initial Commit

---
+++ src/test/java/com/pikacoder/spring/swagger/SpringRestSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pikacoder.spring.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
